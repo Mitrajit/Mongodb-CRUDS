05c579b21c02c257e108ec41fc33aa8dcb547d66
Mitrajit Chandra Chandra <chandra.rupam@gmail.com>
2021-09-02 19:40:27 +0530

    Initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,104 @@
+
+// Activate tooltip
+$('[data-toggle="tooltip"]').tooltip();
+
+// Select/Deselect checkboxes
+var checkedids = [];
+var checkbox = $('table tbody input[type="checkbox"]');
+$("#selectAll").click(function () {
+  if (this.checked) {
+    checkbox.each(function () {
+      this.checked = true;
+      checkedids.push($(this.closest('tr')).data('id'));
+    });
+  } else {
+    checkedids = [];
+    checkbox.each(function () {
+      this.checked = false;
+    });
+  }
+});
+checkbox.click(function () {
+  if (!this.checked) {
+    $("#selectAll").prop("checked", false);
+    let index = checkedids.indexOf($(this.closest('tr')).data('id'))
+    if (index != -1)
+      checkedids.splice(index, 1);
+  }
+  else
+    checkedids.push($(this.closest('tr')).data('id'));
+});
+
+$(".delete").click((e) => {
+  console.log(e.currentTarget.parentNode);
+  Delete([$(e.currentTarget.closest('tr')).data('id')]);
+});
+$('#deleteall').click(e=>Delete(checkedids));
+function Delete(id) {
+  if (confirm("Do you want to permanently delete this record?")) {
+    fetch('/delete', {
+      method: 'delete',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ id })
+    })
+      .then(res => {
+        if (res.ok) return res.status
+      })
+      .then(response => {
+        if (response == 202) {
+          window.location.reload(true);
+        }
+      })
+      .catch(console.error);
+  }
+}
+
+$('#editUserModal').on('show.bs.modal', function (e) {
+
+  //get data-id attribute of the clicked element
+  let row = e.relatedTarget.closest('tr');
+  let Id = $(row).data('id');
+  let children = row.children;
+  //populate the textbox
+  $(e.currentTarget).find('input[name="id"]').val(Id);
+  $(e.currentTarget).find('input[name="name"]').val(children[2].innerText);
+  $(e.currentTarget).find('input[name="phone"]').val(children[3].innerText);
+  $(e.currentTarget).find('input[name="email"]').val(children[4].innerText);
+  $(e.currentTarget).find('input[name="hobbies"]').val(children[5].innerText);
+});
+$('#editUserForm').submit(e => {
+  e.preventDefault();
+  const data = new FormData(e.target);
+  const value = Object.fromEntries(data.entries());
+  fetch('/update', {
+    method: 'put',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(value)
+  }).then(res => {
+    if (res.status == 202)
+      window.location.reload(true);
+  });
+  $('#editUserModal').modal('hide');
+});
+
+$('#sendmail').click(e=>{
+  if(checkedids.length==0)
+  alert("No item selected to send");
+  else
+  {
+    let mailbody="The requested user details are as follows:\n\n";
+    Array.from($('table tbody').children()).forEach(row=>{
+      console.log(row);
+      if($(row.children[0]).find('input[type="checkbox"]').prop('checked')){
+        console.log("True");
+        mailbody+="ID: "+row.children[1].innerText+"\n";
+        mailbody+="Name: "+row.children[2].innerText+"\n";
+        mailbody+="Phone: "+row.children[3].innerText+"\n";
+        mailbody+="Email: "+row.children[4].innerText+"\n";
+        mailbody+="Hobbies: "+row.children[5].innerText+"\n\n";
+    }
+    });
+    console.log(mailbody);
+    window.open("mailto:info@redpositive.in?subject=Users details&body="+encodeURIComponent(mailbody));
+  }
+  });
---
+++ server.js
@@ -0,0 +1,74 @@
+require("dotenv").config();
+const cors = require("cors");
+const express = require("express");
+const mongodb = require("mongodb");
+const { MongoClient } = mongodb;
+const app = express();
+app.use(express.urlencoded({
+    extended: true
+}));
+app.use(express.json());
+
+let whitelist = process.env.ORIGIN.split(' ');
+let corsOptions = {
+    origin: function (origin, callback) {
+        if (!origin || whitelist.indexOf(new URL(origin).hostname) !== -1) {
+            callback(null, true)
+        } else {
+            callback(new Error('Not allowed by CORS'))
+        }
+    },
+    optionsSuccessStatus: 200
+}
+app.use(cors(corsOptions)); // Used CORS to secure origin of API requests
+
+app.set('view engine', 'ejs');
+app.use(express.static('assets'));
+
+// Connect to MongoDB
+MongoClient.connect(process.env.MONGODB_CONNECTION_STRING, {
+    useUnifiedTopology: true
+}).then(client => {
+    console.log('Connected to Database');
+    const db = client.db();
+    const Details = db.collection('Details')
+    app.post("/create", (req, res) => {  //Create
+        Details.insertOne(req.body)
+            .then(result => {
+                res.redirect('/');
+            })
+            .catch(error => console.error(error));
+    });
+    app.get('/', (req, res) => { // Read
+        Details.find().toArray().then(users => {
+            res.render('index.ejs', { users });
+        });
+    });
+    app.put('/update', (req, res) => { // Update
+        Details.updateOne({ _id: new mongodb.ObjectId(req.body.id) },
+            { $set: { name: req.body.name, phone: req.body.phone, email:req.body.email, hobbies:req.body.hobbies}
+        }).then(result=>{
+            if(result.modifiedCount!=0)
+                res.sendStatus(202);
+            else
+                res.sendStatus(204);
+        }).catch(error => console.error(error));
+    });
+    app.delete('/delete', (req, res) => { // Delete
+        let deletionids=[];
+        req.body.id.forEach(element => {
+            deletionids.push(new mongodb.ObjectId(element));
+        });
+        Details.deleteMany({ _id: {$in: deletionids} })
+            .then(result => {
+                if (result.deletedCount == 0) {
+                    return res.sendStatus(200);
+                }
+                else
+                    res.sendStatus(202); //deleted
+            })
+            .catch(error => console.error(error));
+    });
+})
+    .catch(error => console.error(error))
+app.listen(process.env.PORT, () => console.log(`Listening on Port ${process.env.PORT}`));
